48a0f8f1b5c7e31c8e8d03ac9b74a8c6d7043b4f
Admin <Admin@TEB123020>
2023-09-29 14:51:43 +0200

    Add project files.

---
+++ inlämning 2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Inlämnings_Uppgift_2._4
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in månadslönen för dina tre anställda på var sin rad.");
+            int lön1 = int.Parse(Console.ReadLine());
+            int lön2 = int.Parse(Console.ReadLine());
+            int lön3 = int.Parse(Console.ReadLine());
+            double medellön = (lön1 + lön2 + lön3) / 3.0;
+            Console.WriteLine($"De anställdas medellön är {medellön} kr/ månad.");
+            Console.ReadKey();  
+        }
+    }
+}
